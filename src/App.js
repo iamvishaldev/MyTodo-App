a5b3048bdba1fba8dd241c8f9e6c462d7624dd46
Vishal Yadav <vishalyadavcodepur@gmail.com>
2020-10-20 09:27:30 +0530

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container vw-100 vh-100">
+      <div className="mainStyle vh-100">
+        <Todo />
+      </div>
     </div>
   );
 }
